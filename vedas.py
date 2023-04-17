1d606094d7de7805299b26e305b8ae488fb63e07
shahx272 <shahx272@umn.edu>
2023-04-17 16:56:40 -0500

    first

---
+++ vedas.py
@@ -0,0 +1,156 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+
+
+st.set_page_config(page_title='Vedas Transcription', layout="wide", initial_sidebar_state="auto", menu_items=None)
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+#st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+# KrutiDev to Unicode function
+def kd2u(krutidev_substring):
+    
+    modified_substring = krutidev_substring
+    
+    array_one = ["ñ","Q+Z","sas","aa",")Z","ZZ","‘","’","“","”",
+    
+    "å",  "ƒ",  "„",   "…",   "†",   "‡",   "ˆ",   "‰",   "Š",   "‹", 
+    
+    "¶+",   "d+", "[+k","[+", "x+",  "T+",  "t+", "M+", "<+", "Q+", ";+", "j+", "u+",
+    "Ùk", "Ù", "Dr", "–", "—","é","™","=kk","f=k",  
+    
+    "à",   "á",    "â",   "ã",   "ºz",  "º",   "í", "{k", "{", "=",  "«",   
+    "Nî",   "Vî",    "Bî",   "Mî",   "<î", "|", "K", "}",
+    "J",   "Vª",   "Mª",  "<ªª",  "Nª",   "Ø",  "Ý", "nzZ",  "æ", "ç", "Á", "xz", "#", ":",
+    
+    "v‚","vks",  "vkS",  "vk",    "v",  "b±", "Ã",  "bZ",  "b",  "m",  "Å",  ",s",  ",",   "_",
+    
+    "ô",  "d", "Dk", "D", "[k", "[", "x","Xk", "X", "Ä", "?k", "?",   "³", 
+    "pkS",  "p", "Pk", "P",  "N",  "t", "Tk", "T",  ">", "÷", "¥",
+    
+    "ê",  "ë",   "V",  "B",   "ì",   "ï", "M+", "<+", "M",  "<", ".k", ".",    
+    "r",  "Rk", "R",   "Fk", "F",  ")", "n", "/k", "èk",  "/", "Ë", "è", "u", "Uk", "U",   
+    
+    "i",  "Ik", "I",   "Q",    "¶",  "c", "Ck",  "C",  "Hk",  "H", "e", "Ek",  "E",
+    ";",  "¸",   "j",    "y", "Yk",  "Y",  "G",  "o", "Ok", "O",
+    "'k", "'",   "\"k",  "\"",  "l", "Lk",  "L",   "g", 
+    
+    "È", "z", 
+    "Ì", "Í", "Î",  "Ï",  "Ñ",  "Ò",  "Ó",  "Ô",   "Ö",  "Ø",  "Ù","Ük", "Ü",
+    
+    "‚",    "ks",   "kS",   "k",  "h",    "q",   "w",   "`",    "s",    "S",
+    "a",    "¡",    "%",     "W",  "•", "·", "∙", "·", "~j",  "~", "\\","+"," ः",
+    "^", "*",  "Þ", "ß", "(", "¼", "½", "¿", "À", "¾", "A", "-", "&", "&", "Œ", "]","~ ","@"]
+    
+    array_two = ["॰","QZ+","sa","a","र्द्ध","Z","\"","\"","'","'",
+    
+    "०",  "१",  "२",  "३",     "४",   "५",  "६",   "७",   "८",   "९",   
+    
+    "फ़्",  "क़",  "ख़", "ख़्",  "ग़", "ज़्", "ज़",  "ड़",  "ढ़",   "फ़",  "य़",  "ऱ",  "ऩ",    
+    "त्त", "त्त्", "क्त",  "दृ",  "कृ","न्न","न्न्","=k","f=",
+    
+    "ह्न",  "ह्य",  "हृ",  "ह्म",  "ह्र",  "ह्",   "द्द",  "क्ष", "क्ष्", "त्र", "त्र्", 
+    "छ्य",  "ट्य",  "ठ्य",  "ड्य",  "ढ्य", "द्य", "ज्ञ", "द्व",
+    "श्र",  "ट्र",    "ड्र",    "ढ्र",    "छ्र",   "क्र",  "फ्र", "र्द्र",  "द्र",   "प्र", "प्र",  "ग्र", "रु",  "रू",
+    
+    "ऑ",   "ओ",  "औ",  "आ",   "अ", "ईं", "ई",  "ई",   "इ",  "उ",   "ऊ",  "ऐ",  "ए", "ऋ",
+    
+    "क्क", "क", "क", "क्", "ख", "ख्", "ग", "ग", "ग्", "घ", "घ", "घ्", "ङ",
+    "चै",  "च", "च", "च्", "छ", "ज", "ज", "ज्",  "झ",  "झ्", "ञ",
+    
+    "ट्ट",   "ट्ठ",   "ट",   "ठ",   "ड्ड",   "ड्ढ",  "ड़", "ढ़", "ड",   "ढ", "ण", "ण्",   
+    "त", "त", "त्", "थ", "थ्",  "द्ध",  "द", "ध", "ध", "ध्", "ध्", "ध्", "न", "न", "न्",    
+    
+    "प", "प", "प्",  "फ", "फ्",  "ब", "ब", "ब्",  "भ", "भ्",  "म",  "म", "म्",  
+    "य", "य्",  "र", "ल", "ल", "ल्",  "ळ",  "व", "व", "व्",   
+    "श", "श्",  "ष", "ष्", "स", "स", "स्", "ह", 
+    
+    "ीं", "्र",    
+    "द्द", "ट्ट","ट्ठ","ड्ड","कृ","भ","्य","ड्ढ","झ्","क्र","त्त्","श","श्",
+    
+    "ॉ",  "ो",   "ौ",   "ा",   "ी",   "ु",   "ू",   "ृ",   "े",   "ै",
+    "ं",   "ँ",   "ः",   "ॅ",  "ऽ", "ऽ", "ऽ", "ऽ", "्र",  "्", "?", "़",":",
+    "‘",   "’",   "“",   "”",  ";",  "(",    ")",   "{",    "}",   "=", "।", ".", "-",  "µ", "॰", ",","् ","/"]
+    
+    array_one_length = len(array_one)
+    
+    # Specialty characters
+    
+    # Move "f"  to correct position and replace
+    modified_substring = "  " + modified_substring + "  "
+    position_of_f = modified_substring.rfind("f")
+    while (position_of_f != -1):    
+        modified_substring = modified_substring[:position_of_f] + modified_substring[position_of_f+1] + modified_substring[position_of_f] +  modified_substring[position_of_f+2:]
+        position_of_f = modified_substring.rfind("f",0, position_of_f - 1 ) # search for f ahead of the current position.
+    modified_substring = modified_substring.replace("f","ि")
+    modified_substring = modified_substring.strip()
+    
+    # Move "half R"  to correct position and replace
+    modified_substring = "  " + modified_substring + "  "
+    position_of_r = modified_substring.find("Z")
+    set_of_matras =  ["‚",    "ks",   "kS",   "k",     "h",    "q",   "w",   "`",    "s",    "S", "a",    "¡",    "%",     "W",   "·",   "~ ", "~"]
+    while (position_of_r != -1):    
+        modified_substring = modified_substring.replace("Z","",1)
+        if modified_substring[position_of_r - 1] in set_of_matras:
+            modified_substring = modified_substring[:position_of_r - 2] + "j~" + modified_substring[position_of_r - 2:]
+        else:
+            modified_substring = modified_substring[:position_of_r - 1] + "j~" + modified_substring[position_of_r - 1:]
+        position_of_r = modified_substring.find("Z")
+    modified_substring = modified_substring.strip()
+    
+    # Replace ASCII with Unicode
+    for input_symbol_idx in range(0, array_one_length):
+        modified_substring = modified_substring.replace(array_one[input_symbol_idx ] , array_two[input_symbol_idx] )
+    
+    
+    return modified_substring
+
+with open("vedas.txt", encoding='utf8') as file:
+    text = file.read()
+verses = text.split('Verse ')
+
+vdf = pd.DataFrame(verses, columns=['verse'])
+## splits within-verse on new line
+vdf2 = vdf.verse.str.split('Elucidation', expand=True)
+## drop first row, not a verse
+vdf2.drop([0], axis=0, inplace=True)
+## split column 1 on "Practical Utility" and reappend to vdf2
+vdfexp = vdf2[1].str.split('Practical Utility in life', expand=True) ## 0 is elucidation, 1 is utility
+## drop the elucidation column from vdf2
+vdf2temp = vdf2.drop([1], axis=1, inplace=False)
+## vdf3 has verses, elucidation, utility as 0,1,2 cols respectively
+vdf3 = pd.concat([vdf2temp, vdfexp], axis=1, ignore_index=True)
+vdf3.reset_index(inplace=True, drop=True)
+## splitting 0 column
+vdf3_0 = vdf3[0].str.split('\n', expand=True)
+vdf3_0 = vdf3_0.drop([1,2], axis=1).reset_index(drop=True)
+## combine verse, add to original dataframe
+## verse in English lives in columns 3-4
+vdf3['hierarchy'] = vdf3_0[0]
+vdf3['verse'] = vdf3_0[3] + ' ' + vdf3_0[4]
+## drop the long meaningless string
+vdf4 = vdf3.drop([0], axis=1, inplace=False)
+
+## combine meanings
+## drop all columns accounted for already - hierarchy and verse
+vdf3_1 = vdf3_0.drop([0,3,4], axis=1, inplace=False)
+## aggregate meanings
+vdf4['meaning'] = vdf3_1.astype(str).agg(' '.join,axis=1)
+vdf4.columns = ['elu','pu','hier','verse','meaning']
+vdf4['elu'] = vdf4['elu'].str.replace('\n', ' ')
+vdf4['pu'] = vdf4['pu'].str.replace('\n',' ')
+vdf4 = vdf4[['hier','verse','meaning','elu','pu']]
+
+st.dataframe(vdf4)
+
+## Button for downloading vedas data to CSV
+vedas_csv = convert_df(vdf4)
+st.download_button("Download file of all interpretations in text format", vedas_csv, "vedas.csv", "text/csv", key='download-csv')
